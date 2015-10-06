ca8028d601a26fe75f6e70e3b37e880422f9c6cb
AmitGoelNYC <good2speed@yahoo.com>
2015-10-05 20:55:04 -0400

    Code Cleanup and add comments

--- app/src/main/java/com/example/tyson/vendingmachine/MainActivity.java
+++ app/src/main/java/com/example/tyson/vendingmachine/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.tyson.vendingmachine;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
@@ -17,11 +19,12 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
 
+    public static final int VENDING_QUANTITY = 20;
     EditText mDepositText;
     TextView mChangeReturned;
-    VendingMachine vendingMachine;
-    Spinner spinner;
-    int itemSelected;
+    VendingMachine mVendingMachine;
+    Spinner mSpinner;
+    int mItemSelected;
     ArrayList<VendingItem> vendingItems;
 
     @Override
@@ -30,43 +33,61 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         setContentView(R.layout.activity_main);
 
         mDepositText = (EditText) findViewById(R.id.depositAmount);
-        spinner = (Spinner) findViewById(R.id.spinner);
+        mSpinner = (Spinner) findViewById(R.id.spinner);
+        //arraylist to display vending item and price in spinner
         vendingItems = new ArrayList<>();
-
+        //edittext to display change returned
         mChangeReturned = (TextView) findViewById(R.id.textView);
+        //instantiate our Vending Machine object
 
-        vendingMachine = new VendingMachine(100);
+        //get String Array Resources for display and vending item creation purposes
         String[] itemNames = getResources().getStringArray(R.array.item_arrays);
         String[] itemPrice = getResources().getStringArray(R.array.item_costs);
+        //Local String array to help format spinner item display
         List<String> itemsDisplay = new ArrayList<>();
 
+        //for loop to add to display item list and to add vending items to our Vending machine
+        //also initializes price for each vending item
         for(int i = 0; i< itemNames.length; i++){
             itemsDisplay.add(itemNames[i] + " - " + itemPrice[i]);
             double price = Double.parseDouble(itemPrice[i]);
-            vendingItems.add(new VendingItem(price, itemNames[i], 20));
+            vendingItems.add(new VendingItem(price, itemNames[i], VENDING_QUANTITY));
         }
+        mVendingMachine = new VendingMachine(100, vendingItems);
 
+        //sets spinner using arrayAdapter
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, itemsDisplay);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(dataAdapter);
-        spinner.setOnItemSelectedListener(this);
-        vendingMachine.addItems(vendingItems);
+        mSpinner.setAdapter(dataAdapter);
+        mSpinner.setOnItemSelectedListener(this);
     }
 
     public void purchaseItem(View view) {
+        //Code to hide Keyboard on button Press
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
         TextView textTotalChange = (TextView) findViewById(R.id.textTotalChange);
+
+        //logic to process purchases
+        //condition to verify if user has deposited money into the machine and if
+        //amount deposited is enough to make purchase
         if(!TextUtils.isEmpty(mDepositText.getText().toString())) {
             double depositAmount = Double.parseDouble(mDepositText.getText().toString());
             if (depositAmount > 0) {
-                if (vendingMachine.purchaseItem(vendingItems.get(itemSelected), depositAmount)) {
-                    Toast.makeText(this, vendingItems.get(itemSelected).mName + " Dispensed", Toast.LENGTH_LONG).show();
+                if (mVendingMachine.purchaseItem(vendingItems.get(mItemSelected), depositAmount)) {
+                    //inform user that item has been purchased
+                    Toast.makeText(this, vendingItems.get(mItemSelected).mName + " Dispensed", Toast.LENGTH_LONG).show();
                     String returnChange = "";
-                    for(int i = 0; i < vendingMachine.coinsArrayList.size();i++){
-                        returnChange += "1 " + vendingMachine.coinsArrayList.get(i) +" ";
+                    for(int i = 0; i < mVendingMachine.mCoinsArrayList.size();i++){
+                        returnChange += "1 " + mVendingMachine.mCoinsArrayList.get(i) +" ";
                     }
+                    //displays returned change value
                     mChangeReturned.setText(returnChange);
-                    textTotalChange.setText(String.valueOf(vendingMachine.returnMoney));
+                    textTotalChange.setText(String.valueOf(mVendingMachine.mReturnMoney));
                     mDepositText.setText("");
                 } else {
                     Toast.makeText(this, "Please enter more money", Toast.LENGTH_LONG).show();
@@ -79,7 +100,8 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        itemSelected = position;
+        //global variable to get spinner selection position
+        mItemSelected = position;
     }
 
     @Override
--- app/src/main/java/com/example/tyson/vendingmachine/VendingMachine.java
+++ app/src/main/java/com/example/tyson/vendingmachine/VendingMachine.java
@@ -6,20 +6,17 @@ public class VendingMachine implements VendingMachineState {
 
     public static double mTotalMoney;
     ArrayList<VendingItem> mItems;
-    public static ArrayList<Coins> coinsArrayList;
-    double returnMoney = 0;
+    public static ArrayList<Coins> mCoinsArrayList;
+    double mReturnMoney = 0;
 
-    public VendingMachine(double money) {
+    public VendingMachine(double money, ArrayList<VendingItem> items) {
         this.mTotalMoney = money;
-        mItems = new ArrayList<>();
+        this.mItems = items;
     }
 
-    public void addItems(ArrayList<VendingItem> item) {
-        mItems = item;
-    }
-
+    //enum class to represent our various coins
     public enum Coins {
-        NICKEL(.05), DIME(.10), QUARTER(.25), DOLLARCOIN(1.00);
+        NICKEL(.05), DIME(.10), QUARTER(.25), DOLLARCOIN(1.00), PENNY(.01);
         public double value;
 
         Coins(double value) {
@@ -29,15 +26,16 @@ public class VendingMachine implements VendingMachineState {
 
     public boolean purchaseItem(VendingItem item, double amtDeposited) {
         if (amtDeposited > item.mPrice) {
+            //check to see if stock is running low, if so replenish stock
             if (item.mQuantity < 4)
                 restock(item);
             double cost = item.mPrice;
             mTotalMoney -= cost;
-            returnMoney = amtDeposited - cost;
-            returnMoney *= 100;
-            returnMoney = Math.round(returnMoney);
-            returnMoney /= 100;
-            dispenseItem(item, returnMoney);
+            mReturnMoney = amtDeposited - cost;
+            mReturnMoney *= 100;
+            mReturnMoney = Math.round(mReturnMoney);
+            mReturnMoney /= 100;
+            dispenseItem(item, mReturnMoney);
             return true;
         }
         return false;
@@ -45,31 +43,42 @@ public class VendingMachine implements VendingMachineState {
 
     private void dispenseItem(VendingItem item, double returnMoney) {
         item.mQuantity--;
-        coinsArrayList = new ArrayList<>();
+        mCoinsArrayList = new ArrayList<>();
+
+        //logic to return coins by adding them to a list
         while (returnMoney >= Coins.DOLLARCOIN.value) {
-            coinsArrayList.add(Coins.DOLLARCOIN);
+            mCoinsArrayList.add(Coins.DOLLARCOIN);
             returnMoney -= Coins.DOLLARCOIN.value;
+            //rounding error, so multiply a 100 and use MAth.round function
+            //then divide by 100 to attempt to get correct results
             returnMoney *= 100;
             returnMoney = Math.round(returnMoney);
             returnMoney /= 100;
         }
         while (returnMoney >= Coins.QUARTER.value) {
-            coinsArrayList.add(Coins.QUARTER);
+            mCoinsArrayList.add(Coins.QUARTER);
             returnMoney -= Coins.QUARTER.value;
             returnMoney *= 100;
             returnMoney = Math.round(returnMoney);
             returnMoney /= 100;
         }
         while (returnMoney >= Coins.DIME.value) {
-            coinsArrayList.add(Coins.DIME);
+            mCoinsArrayList.add(Coins.DIME);
             returnMoney -= Coins.DIME.value;
             returnMoney *= 100;
             returnMoney = Math.round(returnMoney);
             returnMoney /= 100;
         }
         while (returnMoney >= Coins.NICKEL.value) {
-            coinsArrayList.add(Coins.NICKEL);
+            mCoinsArrayList.add(Coins.NICKEL);
             returnMoney -= Coins.NICKEL.value;
+            returnMoney *= 100;
+            returnMoney = Math.round(returnMoney);
+            returnMoney /= 100;
+        }
+        while (returnMoney >= Coins.PENNY.value) {
+            mCoinsArrayList.add(Coins.PENNY);
+            returnMoney -= Coins.PENNY.value;
             returnMoney *= 100;
             returnMoney = Math.round(returnMoney);
             returnMoney /= 100;
